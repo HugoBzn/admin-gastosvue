a5d26b228a31c3c3cd298e503786838cd7e10f5c
HugoBzn <hugobazan1499@gmail.com>
2023-06-20 21:48:08 -0600

    Modal working open and close with custom event

---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<script setup>
+import cerrarModal from "../assets/img/cerrar.svg";
+
+const emit = defineEmits(["ocultar-modal"]);
+</script>
+
+<template>
+  <div class="modal">
+    <div class="cerrar-modal">
+      <img :src="cerrarModal" alt="ico cerrar modal" @click="$emit('ocultar-modal')" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: absolute;
+  background-color: rgb(0 0 0/0.9);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.cerrar-modal {
+  position: absolute;
+  right: 3rem;
+  top: 3rem;
+}
+
+.cerrar-modal img {
+  width: 3rem;
+  cursor: pointer;
+}
+</style>
