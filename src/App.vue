b8e72c1a4dbbba9f145ed0bda6d9ba9b0464bd1b
HugoBzn <hugobazan1499@gmail.com>
2023-06-26 15:15:09 -0600

    Data with localstorage gastos and balance

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, watch, computed } from "vue";
+import { ref, reactive, watch, computed, onMounted } from "vue";
 
 import Presupuesto from "./components/Presupuesto.vue";
 import ControlPresupuesto from "./components/ControlPresupuesto.vue";
@@ -37,6 +37,8 @@ watch(
     const totalGastado = gastos.value.reduce((total, gasto) => gasto.cantidad + total, 0);
     gastado.value = totalGastado;
     disponible.value = presupuesto.value - totalGastado;
+
+    localStorage.setItem("gastos", JSON.stringify(gastos.value));
   },
   {
     deep: true,
@@ -54,6 +56,23 @@ watch(
     deep: true,
   }
 );
+
+watch(presupuesto, () => {
+  localStorage.setItem("presupuesto", presupuesto.value);
+});
+
+onMounted(() => {
+  const presupuestoStorage = localStorage.getItem("presupuesto");
+  if (presupuestoStorage) {
+    presupuesto.value = Number(presupuestoStorage);
+    disponible.value = Number(presupuestoStorage);
+  }
+
+  const gastosStorage = localStorage.getItem("gastos");
+  if (gastosStorage) {
+    gastos.value = JSON.parse(gastosStorage);
+  }
+});
 
 const reiniciarStateGasto = () => {
   // Reiniciando el formulario
